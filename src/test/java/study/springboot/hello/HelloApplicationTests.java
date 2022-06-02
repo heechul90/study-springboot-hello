2535f0c4686edee35635239e29c70aee25ecc09f
hclee <heechul4296@gmail.com>
2022-06-03 03:38:05 +0900

    source upload 초기소스

---
+++ src/test/java/study/springboot/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
