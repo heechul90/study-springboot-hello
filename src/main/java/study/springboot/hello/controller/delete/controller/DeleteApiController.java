2ccb0da3c3f8ec0bf7c20ee09df4ec916980a03c
hclee <heechul4296@gmail.com>
2022-06-03 11:53:27 +0900

    delete api

---
+++ src/main/java/study/springboot/hello/controller/delete/controller/DeleteApiController.java
@@ -0,0 +1,17 @@
+package study.springboot.hello.controller.delete.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/delete")
+public class DeleteApiController {
+
+    @DeleteMapping(value = "{userId}")
+    public String delete(@PathVariable("userId") Long id) {
+        System.out.println("id = " + id);
+        return "ok";
+    }
+}
